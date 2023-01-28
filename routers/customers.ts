78841ae1e582cf7c9fafa594928a88b25cf9cda9
dennisappiahALX <appiahdennis1998@gmail.com>
2023-01-28 12:57:11 +0000

    implemented customers api

--- routers/customers.ts
+++ routers/customers.ts
@@ -4,29 +4,58 @@ import {Router} from "express"
 const router = Router()
 
 
-router.get('/', (req, res) => {
-   
+router.get('/', async(req, res) => {
+   const customers = await Customer.find().sort('name');
+   res.status(200).send(customers);
 });
 
 
-router.post('/', (req, res) => {
-    
+router.post('/', async(req, res) => {
+    const {error} = validateCustomer(req.body);
+    if (error)  return res.status(400).send(error.details[0].message);
+
+    const customer = new Customer( {
+        name: req.body.name,
+        phone : req.body.phone,
+        isGold : req.body.isGold,
+    });
+
+    await customer.save();
+
+    res.status(201).send(customer);
+
 });
 
-router.get('/:id', (req, res) => {
-    
+router.get('/:id',async (req, res) => {
+    const customer= await Customer.findById(req.params.id);
+
+    if (!customer) return res.status(404).send('The customer with the given ID was not found!');
+
+    res.status(200).send(customer);
 })
 
-router.put('/:id', (req, res) => {
-    
+router.put('/:id', async(req, res) => {
+    const {error} = validateCustomer(req.body);
+    if (error)  return res.status(400).send(error.details[0].message);
+
+    const customer = await Customer.findByIdAndUpdate(req.params.id, {
+        name :req.body.name,
+        isGold : req.body.isGold,
+        phone : req.body.phone }, { new : true });
+
+    if (!customer) return res.status(404).send('The customer with the given ID was not found!');
+
+    res.status(200).send(customer); 
 });
 
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
+    const customer = await Customer.findByIdAndRemove(req.params.id);
+
+    if (!customer) return res.status(404).send('The customer with the given ID was not found!');
     
+    res.status(200).send(customer);
 });
-
-
 
 
 export default router
