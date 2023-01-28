e3066338f094e2530ed9893a456b37f5fe9778e1
dennisappiahALX <appiahdennis1998@gmail.com>
2023-01-28 10:18:47 +0000

    implement customer dtos

--- dtos/create-book.ts
+++ dtos/create-book.ts
@@ -1,3 +1,8 @@
+import CategoryDto from "./create-category"
+
 export default interface createBookDto {
     title: string
+    category: CategoryDto
+    numberInStock: number
+    dailyRentalRate: number
 }
---
+++ dtos/create-course.ts
@@ -0,0 +1,6 @@
+import CategoryDto from "./create-category"
+
+export default interface createCourseDto {
+    name: string
+    // category: CategoryDto
+}
