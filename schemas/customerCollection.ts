5c8360f12fd8f2336b5ce7a02d679c87bde8750e
dennisappiahALX <appiahdennis1998@gmail.com>
2023-01-28 10:20:30 +0000

    implement model schemas

---
+++ schemas/customerCollection.ts
@@ -0,0 +1,32 @@
+import Joi from "joi"
+import mongoose from "mongoose"
+import CustomerModel from './../models/customer';
+
+export const Customer = mongoose.model('Customer', new mongoose.Schema({
+    name : { 
+        type : String, 
+        required : true, 
+        minlength : 5 ,
+         maxlength : 50},
+    isGold : {
+        type : Boolean,
+        default : false
+    },
+    phone : { 
+        type : String, 
+        required : true, 
+        minlength : 5 , 
+        maxlength : 50}
+}));
+
+export const validateCustomer = (customer: CustomerModel) => {
+    const schema = Joi.object({ 
+        name: Joi.string().min(5).max(50).required(),
+        isGold : Joi.boolean(),
+        phone: Joi.string().min(5).max(50).required()
+        });
+    
+    return schema.validate(customer);
+}
+
+
