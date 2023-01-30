657d5f2293b75292908d3026e2f0c96b83b763ea
dennisappiahALX <appiahdennis1998@gmail.com>
2023-01-30 18:44:20 +0000

    middleware function to log all errors in application

---
+++ middlewares/logger.ts
@@ -0,0 +1,38 @@
+import {createLogger, transports, format} from "winston"
+import 'winston-mongodb'
+
+const logger = createLogger({
+    transports : [
+        new transports.Console(),
+        new transports.File({
+            level : 'debug',
+            filename : 'logWarning.log'
+        }),
+
+        new transports.File({
+            level : 'info',
+            filename : 'logInfo.log'
+        }),
+        
+        new transports.MongoDB({
+            db : `${process.env.DB}`,
+            options: {useNewUrlParser: true, useUnifiedTopology: true},
+            collection : 'logs',
+            level: 'error'
+        })
+    ],
+
+    exceptionHandlers : [
+        new transports.Console({ handleExceptions: true}) 
+    ],
+
+    format :  format.combine (
+        format.json(),
+        format.timestamp(),
+        format.metadata(),
+        format.prettyPrint(),
+    )
+});
+
+
+export default logger
