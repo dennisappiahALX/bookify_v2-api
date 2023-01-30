0310b4702803f565474783d51afd63bdf9d57371
dennisappiahALX <appiahdennis1998@gmail.com>
2023-01-30 18:49:00 +0000

    server

--- app.ts
+++ app.ts
@@ -1,12 +1,12 @@
-import jwt from "jsonwebtoken"
 import express from "express";
 import { setupRoutes } from './startup/registerRoutes';
 import { setupDb } from "./startup/integrateDb";
 import { setupConfig } from "./startup/setConfig";
+import { setupLogging} from "./startup/setLogging"
 
 const app = express();
 
-
+setupLogging(app)
 setupRoutes(app);
 setupDb();
 setupConfig();
