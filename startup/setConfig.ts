9ee8d939beadf9476eb63888a1091253bdf4262a
dennisappiahALX <appiahdennis1998@gmail.com>
2023-01-31 17:49:40 +0000

    set up environment config

--- startup/setConfig.ts
+++ startup/setConfig.ts
@@ -1,7 +1,15 @@
-import dotenv from 'dotenv'
-dotenv.config()
+import "dotenv/config";
 
-export const setupConfig = ():void => {
-   
-    console.log(`Application Name: ${process.env.NAME}`);
-}
+
+export const configEnv = (): void => {
+  if (process.env.NODE_ENV === "test") {
+    require("dotenv").config({ path: ".env.test" });
+  } else if (process.env.NODE_ENV === "production") {
+    require("dotenv").config({ path: ".env.production" });
+  } else {
+    require("dotenv").config({ path: ".env.development" });
+  }
+};
+
+
+//set NODE_ENV=test
