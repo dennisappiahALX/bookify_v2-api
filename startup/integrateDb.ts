4ef9656258331de832e93668cdd7c583cd905710
dennisappiahALX <appiahdennis1998@gmail.com>
2023-01-31 17:48:44 +0000

    set up test database

--- startup/integrateDb.ts
+++ startup/integrateDb.ts
@@ -1,7 +1,10 @@
 import mongoose from "mongoose"
+import { configEnv } from "./setConfig"
+
+configEnv()
 
 export const setupDb = ():void => {
     mongoose.connect(`${process.env.DB}`)
-       .then(() => console.log('Connected to MongoDb....'))
-       .catch(err => console.log("Could not connect to MongoDb.."))
+       .then(() => console.log(`Connected to ${process.env.DB}`))
+       .catch(err => console.log("Could not connect to MongoDb..", err))
 }
